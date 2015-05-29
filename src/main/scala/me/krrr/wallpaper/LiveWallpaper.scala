05cead8fcfd3a6c0fbf7109ebcb2e3f7b80d379c
krrr <guogaishiwo@gmail.com>
2015-05-29 20:19:00 +0000

    Rewrite wallpaper service

--- src/main/scala/me/krrr/wallpaper/LiveWallpaper.scala
+++ src/main/scala/me/krrr/wallpaper/LiveWallpaper.scala
@@ -15,6 +15,7 @@ import GradientArtDrawable.Filter
 
 import scala.util.Random
 
+
 class LiveWallpaper extends WallpaperService {
     private val handler = new Handler
 
@@ -22,12 +23,25 @@ class LiveWallpaper extends WallpaperService {
 
     class GraEngine extends Engine with OnSharedPreferenceChangeListener {
         private val pref = PreferenceManager.getDefaultSharedPreferences(getApplicationContext)
-        private var drawTask: Runnable = null
-        private val (view, nameLabel, subLabel) = {  // inflate view and set text shadows
+        private var changeTaskDelayed = false
+        private val changeTask = new Runnable {
+            def run() = {
+                if (isVisible) {
+                    fromSettings()
+                    doDrawing()
+                } else {
+                    changeTaskDelayed = true
+                }
+                handler.postDelayed(this, pref.getString("period", "1800000").toInt)
+            }
+        }
+        private val (view, nameLabel, subLabel, gra) = {
+            // inflate view and set text shadows
             val view = LayoutInflater.from(
                 getApplicationContext).inflate(R.layout.main, null)
             val nameLabel = view.findViewById(R.id.gra_name).asInstanceOf[TextView]
             val subLabel = view.findViewById(R.id.gra_subname).asInstanceOf[TextView]
+            val gra = view.findViewById(R.id.gra_view).asInstanceOf[GradientArtView].gra
 
             val metrics = new DisplayMetrics
             val wm = getSystemService(Context.WINDOW_SERVICE).asInstanceOf[WindowManager]
@@ -37,27 +51,41 @@ class LiveWallpaper extends WallpaperService {
             nameLabel.setShadowLayer(radius, 0, 0, 0xEE111111)
             subLabel.setShadowLayer(radius, 0, 0, 0xEE222222)
 
-            (view, nameLabel, subLabel)
+            (view, nameLabel, subLabel, gra)
         }
-        private var period = 0  // millisecond
+        val patterns = {
+            val i_stream = getResources.openRawResource(R.raw.uigradients)
+            val json_s = io.Source.fromInputStream(i_stream).mkString
+            try new JSONArray(json_s) catch {
+                case e: JSONException => null
+            }
+        }
 
         override def onSurfaceCreated(holder: SurfaceHolder) {
             holder.setFormat(PixelFormat.RGBA_8888)
             fromSettings()
             pref.registerOnSharedPreferenceChangeListener(this)
+            // for some unknown reasons, onVisibilityChanged will be called three
+            // times initially: show, hide, show. This is a workaround.
+            handler.postDelayed(changeTask, 500)
         }
 
         override def onSurfaceChanged(holder: SurfaceHolder, format: Int,
-                                      width: Int, height: Int) = layoutView(width, height)
+                                      width: Int, height: Int) {
+            layoutView(width, height)
+        }
 
         override def onSurfaceDestroyed(holder: SurfaceHolder) {
+            handler.removeCallbacks(changeTask)
             pref.unregisterOnSharedPreferenceChangeListener(this)
-            handler.removeCallbacks(drawTask)
         }
 
         override def onVisibilityChanged(visible: Boolean) {
-            if (visible) doDrawing()
-            else handler.removeCallbacks(drawTask)
+            if (visible && changeTaskDelayed) {
+                fromSettings()
+                doDrawing()
+                changeTaskDelayed = false
+            }
         }
 
         private def layoutView(w: Int, h: Int) {
@@ -66,68 +94,47 @@ class LiveWallpaper extends WallpaperService {
             view.layout(0, 0, w, h)
         }
 
+        // Only called after this engine created or settings
+        // changed or called by changeTask repeatedly.
         def doDrawing() {
             val holder = getSurfaceHolder
             val canvas = holder.lockCanvas()
-
             if (canvas != null) {
                 view.draw(canvas)
                 holder.unlockCanvasAndPost(canvas)
             }
-
-            // reschedule the next draw
-            handler.removeCallbacks(drawTask)
-            handler.postDelayed(drawTask, period)
         }
 
         def fromSettings() = {
-            val graView = view.findViewById(R.id.gra_view).asInstanceOf[GradientArtView]
-            val filterIdx = pref.getString("filter", "0").toInt
-            val randomFilterEnabled = filterIdx == -1
-            period = pref.getString("period", "1800000").toInt
+            val idx = pref.getString("filter", "0").toInt
+            gra.filter = Filter(if (idx == -1) Random.nextInt(Filter.maxId) else idx)
 
-            val i_stream = getResources.openRawResource(R.raw.uigradients)
-            val json_s = io.Source.fromInputStream(i_stream).mkString
-            val entries = try new JSONArray(json_s) catch {case e: JSONException => null}
+            var (name, subName) = ("", "")
+            try {
+                val entry = patterns.getJSONObject(Random.nextInt(patterns.length))
+                if (entry.has("color"))
+                    gra.setColor(entry.getString("color"))
+                else
+                    gra.setColors(Array("color1", "color2").map(entry.getString))
 
-            def randomFilter() =
-                graView.gra.filter = Filter(Random.nextInt(Filter.maxId))
-
-            def randomColor() {
-                var (name, subName) = ("", "")
-                try {
-                    val entry = entries.getJSONObject(Random.nextInt(entries.length))
-                    if (entry.has("color"))
-                        graView.gra.setColor(entry.getString("color"))
-                    else
-                        graView.gra.setColors(Array("color1", "color2").map(entry.getString))
-
-                    if (pref.getBoolean("show_name", true)) {
-                        name = entry.getString("name")
-                        subName = if (entry.has("sub_name")) entry.getString("sub_name") else ""
-                    }
-                    nameLabel.setText(name)
-                    subLabel.setText(subName)
-                } catch {
-                    case e @ (_: JSONException | _: NullPointerException) =>
-                        nameLabel.setText("Failed to parse JSON")
-                        subLabel.setText(e.toString) // it has text, just for layout editing
-                }
-                layoutView(view.getWidth, view.getHeight)
+                if (pref.getBoolean("show_name", true)) {
+                    name = entry.getString("name")
+                    subName = if (entry.has("sub_name")) entry.getString("sub_name") else ""
+                }  // else, set there name to empty
+                nameLabel.setText(name)
+                subLabel.setText(subName)
+            } catch {
+                case e@(_: JSONException | _: NullPointerException) =>
+                    nameLabel.setText("Failed to parse JSON")
+                    subLabel.setText(e.toString)
             }
-
-            if (randomFilterEnabled) randomFilter()  // do first time
-            else graView.gra.filter = Filter(filterIdx)
-            randomColor()
-            drawTask = new Runnable {
-                def run() = {
-                    if (randomFilterEnabled) randomFilter()
-                    randomColor()
-                    doDrawing()
-                }
-            }
+            layoutView(view.getWidth, view.getHeight)
         }
 
-        def onSharedPreferenceChanged(pref: SharedPreferences, key: String) = fromSettings()
+        def onSharedPreferenceChanged(pref: SharedPreferences, key: String) {
+            fromSettings()
+            doDrawing()
+        }
     }
+
 }
